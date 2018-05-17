4e9aa4207712a9e83b5626ce32ab6aa9499b03a4
Tu Dinh Xuan <tudx.hhtv@gmail.com>
2018-05-18 01:05:53 +0700

    Add Sticky Navigation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 import 'bootstrap';
 
 import SmoothScroll from 'smoothscroll-for-websites';
-import Carousel from './slick';
+
 import SM from './scrollmagic';
 
 const OM = {};
@@ -16,16 +16,62 @@ OM.parallax = function () {
 	SM.parallax();
 };
 
-OM.showMobileNavigation = function () {
+OM.activeMobileNavigation = function () {
 
-	const hamburgerIcon = "#hamburger-icon";
+	const getBody = $('body');
+	const getHamburgerIcon = $('#hamburger-icon');
+	const getMobileNavigation = $('#o-mobile-navigation');
 
-	if ($(hamburgerIcon)) {
-		$(hamburgerIcon).on('click', function () {
-			$(".o-mobile-navigation").toggleClass('active');
+	if (getHamburgerIcon) {
+		getHamburgerIcon.on('click', function () {
+
+			getMobileNavigation.toggleClass('active');
+			getBody.toggleClass('mobile-navigation');
+
 			return false;
-		})
+		});
 	}
+};
+
+OM.activeStickyNavigation = function () {
+
+	const getSiteHeader = $("#masthead");
+	const getSiteContent = $("#content");
+
+	let lastScrollTop = 0;
+
+	let delta = 1;
+
+	$(document).scroll(function () {
+
+		const nowScrollTop = $(document).scrollTop();
+
+		if (getSiteHeader && getSiteContent) {
+
+			const getSiteHeaderHeight = getSiteHeader.innerHeight();
+
+			if (nowScrollTop - getSiteHeaderHeight > 1) {
+				getSiteHeader.addClass("sticky").css("top", -getSiteHeaderHeight);
+				getSiteContent.css("margin-top", getSiteHeaderHeight);
+
+			} else if (nowScrollTop === 0) {
+				getSiteHeader.removeClass("sticky");
+				getSiteContent.css("margin-top", "");
+			}
+
+			if (nowScrollTop > 1 && Math.abs(lastScrollTop - nowScrollTop) >= delta) {
+
+				if (nowScrollTop > lastScrollTop) {
+					getSiteHeader.css("top", -getSiteHeaderHeight);
+
+				} else {
+					getSiteHeader.css("top", 0);
+				}
+
+				lastScrollTop = nowScrollTop;
+			}
+		}
+	});
 };
 
 $(document).ready(function () {
@@ -33,21 +79,24 @@ $(document).ready(function () {
 	/**
 	 * SmoothScroll
 	 */
-
 	OM.smoothscroll();
 
 	/**
 	 * Parallax
 	 */
-
 	OM.parallax();
 
 
 	/**
 	 * Mobile Navigation
 	 */
+	OM.activeMobileNavigation();
 
-	OM.showMobileNavigation();
+
+	/**
+	 * Sticky Navigation
+	 */
+	OM.activeStickyNavigation();
 });
 
 export default OM;
